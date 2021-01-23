798d5413e4905dc1be74c7957c9cf96797c6f531
David Sutherland <davethecanuck@gmail.com>
2021-01-23 15:19:58 -0700

    Finished framing out node iterators.

--- src/node/iter.rs
+++ src/node/iter.rs
@@ -13,6 +13,7 @@ enum ChildIterator<'a> {
     Node(Box<NodeIterator<'a>>),
 }
 
+// Iterator for run bits
 struct RunIterator {
 }
 
@@ -22,6 +23,16 @@ impl RunIterator {
     }
 }
 
+impl Iterator for RunIterator {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // EYE TBD
+        None
+    }
+}
+
+// Iterator for raw level 0 bits
 struct BitsIterator {
 }
 
@@ -31,23 +42,49 @@ impl BitsIterator {
     }
 }
 
+impl Iterator for BitsIterator {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // EYE TBD
+        None
+    }
+}
+
+// Main iterator for a node
 pub struct NodeIterator<'a> {
     addr: Addr,
     node: &'a Node,
     index_iter: KeyIndexIterator<'a>,
-    key_state: Option<KeyState>,
     child_iter: ChildIterator<'a>,
 }
 
 impl<'a> NodeIterator<'a> {
     pub fn new(node: &'a Node, addr: Addr) -> NodeIterator {
-        let mut index_iter = node.index.iter();
-        let key_state = index_iter.next();
+        NodeIterator {
+            addr, 
+            node,
+            index_iter: node.index.iter(), 
+            child_iter: ChildIterator::End,
+        }
+    }
 
-        let child_iter = match key_state {
+    // Return next item from child iterator
+    fn next_from_child(&mut self) -> Option<u64> {
+        match &mut self.child_iter {
+            ChildIterator::Bits(iter) => iter.next(),
+            ChildIterator::Run(iter) => iter.next(),
+            ChildIterator::Node(iter) => iter.next(),
+            ChildIterator::End => None,
+        }
+    }
+
+    // Return the next child iterator
+    fn update_child_iterator(&mut self) {
+        self.child_iter = match self.index_iter.next() {
             Some(KeyState::Node(key, offset)) => {
                 // Iterator for child node
-                match &node.content {
+                match &self.node.content {
                     Content::Bits(vec) => {
                         let child_bits = vec[offset];
                         ChildIterator::Bits(BitsIterator::new(child_bits))
@@ -56,7 +93,7 @@ impl<'a> NodeIterator<'a> {
                         let child_node = &vec[offset];
                         ChildIterator::Node(
                             Box::new(
-                                child_node.iter(addr.clone())
+                                child_node.iter(self.addr.clone())
                             )
                         )
                     },
@@ -67,60 +104,19 @@ impl<'a> NodeIterator<'a> {
             },
             _ => ChildIterator::End
         };
-    
-        NodeIterator {
-            addr: addr, 
-            node: node,
-            index_iter, 
-            key_state, 
-            child_iter,
-        }
     }
 }
 
 impl<'a> Iterator for NodeIterator<'a> {
-    type Item = Addr;
-    // EYE - Should pass an Addr around, but return
-    // u64 (bitno). This simplifies iterating through
-    // runs
+    // Returns u64, but internally use Addr
+    type Item = u64;
 
     fn next(&mut self) -> Option<Self::Item> {
-        // EYE - Need Addr methods to show the range
-        // of values for a given Addr prefix and level
-        // - do we need different types of iterators?
-        let mut result = None;
-
-        match self.key_state {
-            Some(KeyState::Node(key, offset)) => {
-                match &self.node.content {
-                    Content::Nodes(_vec) => {
-                        // Get iterator for the child
-                        let mut child_addr = self.addr.clone();
-                        child_addr.set(self.node.level() - 1, key);
-                        // EYE - how do we get back here...
-                        // - need to map this out
-                        // - Can't have iterator containing iterator
-                        // as size is undefined (unless we box)
-                        // 
-                        // EYE - Use a NodeContentIterator enum which 
-                        // has iterators for Run, Bits, or Nodes
-                        // - Consider putting modules into src/node/node.rs
-                        //   and src/node/node_test.rs, .../node_iter.rs, etc
-                        // - This gives us private node::iter and node::test submodules
-                    },
-                    Content::Bits(_vec) => {
-                        // EYE - basically need a Bits iterator...
-                    },
-                }
-
-                // EYE need to iterate on the child
-            },
-            Some(KeyState::Run(key)) => {
-                // Use Addr method to get start and
-                // end bitno. Keep this in the iterator
-                // and iterate over the bits.
-            },
-            _ => ()
+        let mut result = self.next_from_child();
+        if result == None {
+            // We've exhausted the current child iterator
+            self.update_child_iterator();
+            result = self.next_from_child();
         }
         result
     }
